91255da103d81eb9a539e4c45d1e7dff4cf48758
selmaviro <selmaviro@gmail.com>
2026-09-17 16:59:05 +0700

    Update panen.py

--- panen.py
+++ panen.py
@@ -1,3 +1,14 @@
-git remote add origin https://github.com/username/sistem-panen.git
-git branch -M main
-git push -u origin main
+# Sistem Pencatatan Hasil Panen Digital
+
+## Deskripsi
+Program sederhana untuk menghitung total hasil panen dan menghitung total setelah diskon.
+
+## Cara Menjalankan Program
+Program dapat dijalankan menggunakan Python dengan perintah:
+
+python panen.py
+
+## Fitur
+- Menghitung total hasil panen
+- Menghitung diskon
+- Menampilkan total setelah diskon# sistem-panen-selma
